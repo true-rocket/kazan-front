c0e3f710f40de5a2a58e7a02ea0b7ef73b6ec31b
evgeniy.merkul <evgeniy.merkul@rightstep.ru>
2019-09-29 02:54:29 +0300

    bs

---
+++ src/components/BackService.vue
@@ -0,0 +1,89 @@
+ import axios from 'axios'
+
+ const HTTP = axios.create({
+   headers: {
+     'Content-Type': 'application/json',
+     'Access-Control-Allow-Origin': '*'
+   },
+   xsrfHeaderName: 'Csrf-Token',
+   withCredentials: true,
+   maxContentLength: 50000000,
+   timeout: 150000
+ })
+
+  export default {
+    name: 'back-service',
+    components: {},
+    props: [],
+    data() {
+      return {}
+    },
+
+    methods: {
+
+      getDataAsync (request) {
+        return HTTP.get(request)
+          .then(resp => {
+            if (resp != 'error') {
+              return Promise.resolve(resp)
+            } else {
+              return Promise.reject(new Error('getting error'))
+            }
+          })
+          .catch(e => {
+            console.log(e)
+          })
+      },
+
+      createAsync (toCreate) {
+        let cnfBody = {
+          data: toCreate
+        }
+        return HTTP.post('/create/' + cnfBody)
+          .then(resp => {
+            if (resp != 'error') {
+              return Promise.resolve(resp)
+            } else {
+              return Promise.reject(new Error('getting error'))
+            }
+          })
+          .catch(e => {
+            console.log(e)
+          })
+      },
+
+      updateAsync (toUpdate) {
+        let cnfBody = {
+          data: toUpdate
+        }
+        return HTTP.post('/upd/' + cnfBody)
+          .then(resp => {
+            if (resp != 'error') {
+              return Promise.resolve(resp)
+            } else {
+              return Promise.reject(new Error('getting error'))
+            }
+          })
+          .catch(e => {
+            console.log(e)
+          })
+      },
+
+      deleteAsync (toDelete) {
+        let cnfBody = {
+          data: toDelete
+        }
+        return HTTP.post('/del/' + cnfBody)
+          .then(resp => {
+            if (resp != 'error') {
+              return Promise.resolve(resp)
+            } else {
+              return Promise.reject(new Error('getting error'))
+            }
+          })
+          .catch(e => {
+            console.log(e)
+          })
+      }
+  }
+}
